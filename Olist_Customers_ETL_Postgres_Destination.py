d12ebef69fdd01b31e1b0207d6dcf119af5bba4f
tult1999 <118616577+tult1999@users.noreply.github.com>
2023-05-22 14:58:13 +0700

    Update and rename Postgres to Olist_Customers_ETL_Postgres_Destination.py

--- Olist_Customers_ETL_Postgres_Destination.py
+++ Olist_Customers_ETL_Postgres_Destination.py
@@ -4,25 +4,26 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime, timedelta
-#COPY {table} FROM {path} DELIMITER ',' CSV HEADER;
-        # COMMIT;
+
 default_args = {
     'start_date': datetime(2023, 5, 21),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-def load_data_to_postgres():
-    pg_hook = PostgresHook(postgres_conn_id='my_postgres_db')
-    file_folder = 'Olist'
-    file_name = 'Customers.csv'
-    table = 'Customers'
+# BLOCK1: Built-in Pythoncallables
+def load_data_to_postgres(conn_id: str, file_folder: str, file_name: str, table: str):
+    pg_hook = PostgresHook(postgres_conn_id=conn_id)
+    file_folder = file_folder
+    file_name = file_name
+    table = table
     path = f'/Datafile/Outputfile/{file_folder}/{file_name}'
 
     query = f"COPY {table} FROM '{path}' DELIMITER ',';"
 
     pg_hook.run(sql=query, autocommit=True)
 
+# BLOCK2: Config Parameters Pythoncallables
 def exec_olist_postgres_destination_load_data():
     if 1 != 0:
         load_data_to_postgres()
