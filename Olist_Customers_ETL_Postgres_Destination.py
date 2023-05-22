75265c176b2f5e9df3e994418d6dc386899f5734
tult1999 <118616577+tult1999@users.noreply.github.com>
2023-05-22 15:11:21 +0700

    Update Olist_Customers_ETL_Postgres_Destination.py

--- Olist_Customers_ETL_Postgres_Destination.py
+++ Olist_Customers_ETL_Postgres_Destination.py
@@ -12,7 +12,22 @@ default_args = {
 }
 
 # BLOCK1: Built-in Pythoncallables
-def load_data_to_postgres(conn_id: str, file_folder: str, file_name: str, table: str):
+def postgres_destination_load_data(conn_id: str, file_folder: str, file_name: str, table: str):
+    """
+    * Main infomation:
+    Name: postgres_destination_load_data
+    Used for: Loading data from a flat .csv file to PostgreSQL Database
+    Conditions to use: 
+        (1) Connection already set-up on Apache Airflow, you can check on the Airflow server with the command 'airflow connections list'
+        (2) PostgreSQL Database server is on
+        (3) The connection works fine
+
+    * Parameters:
+    param conn_id: The connection ID of the connection established between Apache Airflow server and MongoDB database
+    param file_folder: The name of the folder of the imported file
+    param file_name: The name of the imported file
+    param table: The table name to be imported to
+    """
     pg_hook = PostgresHook(postgres_conn_id=conn_id)
     file_folder = file_folder
     file_name = file_name
@@ -23,10 +38,10 @@ def load_data_to_postgres(conn_id: str, file_folder: str, file_name: str, table:
 
     pg_hook.run(sql=query, autocommit=True)
 
-# BLOCK2: Config Parameters Pythoncallables
+# BLOCK2: Config Parameters and Execute Pythoncallables
 def exec_olist_postgres_destination_load_data():
     if 1 != 0:
-        load_data_to_postgres(conn_id='my_postgres_id', file_folder='Olist', file_name='Customers.csv', table='Customers')
+        postgres_destination_load_data(conn_id='my_postgres_id', file_folder='Olist', file_name='Customers.csv', table='Customers')
 
 with DAG('postgres_destination_load_data', schedule_interval='0 0 * * *', default_args=default_args, catchup=False) as dag:
     postgres_destination_load_data = PythonOperator(
